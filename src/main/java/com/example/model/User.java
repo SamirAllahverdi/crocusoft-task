4462bf77d5bb1e50303207d82d7b29782d5c90e3
SamirAllahverdi <a_nazim2007@mail.ru>
2020-09-17 00:40:59 -0700

    --- h2 database for temporary

--- src/main/java/com/example/model/User.java
+++ src/main/java/com/example/model/User.java
@@ -23,7 +23,7 @@ import java.util.Collection;
 public class User implements UserDetails {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private long id;
 
     private String email;
